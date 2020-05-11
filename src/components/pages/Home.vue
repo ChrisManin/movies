d15d47d941fd4900ba59286a0f8b4bb308a00aaa
ChrisManin <tof.giga@hotmail.fr>
2020-05-11 16:42:09 +0200

    initialisation

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,39 +1,12 @@
 <template>
   <div>
     <h1>Liste des films</h1>
-    <div class="row">
-      <div v-for="movie in movies" :key="movie.slug" class="col-sm-6">
-        <MovieItem :movie="movie" />
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import MovieItem from "@/components/MovieItem";
 
-export default {
-  components: {
-    MovieItem,
-  },
-  data() {
-    return {
-      movies: this.$store.state.movies.movies,
-    };
-  },
-  head: function() {
-    return {
-      title: {
-        inner: "Liste des films",
-      },
-      meta: [
-        { name: "description", content: "Liste de bons films" },
-        { name: "application-name", content: "meSupersFilms" },
-        { name: "og-title", content: "Liste des films" },
-      ],
-    };
-  },
-};
+export default {};
 </script>
 
 <style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
-import { store } from "./store";
-import VueHead from "vue-head";
+
 
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
@@ -12,7 +11,6 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import Home from "@/components/pages/Home";
 import Movie from "@/components/pages/Movie";
 
-Vue.use(VueHead);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(VueRouter);
@@ -24,11 +22,9 @@ const routes = [
   { path: "/movie/:slug", component: Movie },
 ];
 
-const router = new VueRouter({ routes });
+const router = new VueRouter({ mode: "history", routes });
 
 new Vue({
   render: (h) => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
-  store,
   router,
 }).$mount("#app");
